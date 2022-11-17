fd66fa0c91d8fff19e0bb71ad8ec629b7718001e
Kashish Garg <kashishgarg9@gmail.com>
2022-11-17 12:51:58 -0500

    Final commit, working just fine

--- src/website_changes.cpp
+++ src/website_changes.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include "../include/rapidcsv.h"
+#include <bits/stdc++.h>
 
 //things to check for in the csv TODO:
 // whether there is a single instance for every Person
@@ -9,6 +10,11 @@
 
 int main() {
   std::cout << "This is a script to automate the people tracking in KumarLab" << std::endl;
+  std::cout << "There are some assumptions that get made in this script:" << std::endl;
+  std::cout << "1. You are providing a csv with two sets of paired columns, one of the entire, current roster of KumarLab" <<std::endl;
+  std::cout << "and one of the current members of the lab that are on KumarRobotics.org, and their respective positions" << std::endl;
+  std::cout << "2. All names are typed exactly the same for matching members (to be fixed in the future)" << std::endl;
+  std::cout << "3. The sizes of the member's names and titles columns are the same (to be fixed in the future)" <<std::endl;
 
   std::vector<std::string> output_website_directions;
   rapidcsv::Document doc("src/lab_tracking.csv");
@@ -17,55 +23,66 @@ int main() {
   std::vector<std::string> current_title_col = doc.GetColumn<std::string>("current_title");
   std::vector<std::string> website_title_col = doc.GetColumn<std::string>("website_title");
 
+  std::vector<int> website_index_to_ignore;
+
   
   for (int i= 0 ; i< current_person_col.size(); i++) {
     bool name_matched = false;
-    std::cout << "Current Person: " << current_person_col[i] << std::endl;
     const std::string current_person_val = current_person_col[i];
     const std::string current_title_val = current_title_col[i];
 
     for (int j = 0; j < website_person_col.size(); j++) {
-      std::cout << "Website Member: " << website_person_col[j] << std::endl; 
       const std::string website_person_val = website_person_col[j];
       const std::string website_title_val = website_title_col[j];
 
       if (current_person_val == website_person_val) {
-        //want to remove member from both vectors:
-        //website_person_col.erase(website_person_col.begin()+(j-1));
-        //current_person_col.erase(current_person_col.begin()+(i-1));
-        //
-        std::cout << "We got a name match!" <<std::endl; 
+
         name_matched = true;
 
         // check for position match:
-        std::cout << "Current Title: " << current_title_val << std::endl;
-        std::cout << "Website Title: " << website_title_val << std::endl;
 
         if (current_title_val == website_title_val) {
-          std::cout << "Do not move this individual from the current members of the lab on the website" << std::endl;
         } else {
           // if the titles do not match, we need to perform an operation here:
           // Need to MOVE current_person_val FROM 
-          std::string output("MOVE," + current_person_val + "," + "Current_Members" +","+ website_title_val + "," + "Current_Members" + "," + current_title_val);
+          std::string output("MOVE," + current_person_val + "," + "Current Members" +","+ website_title_val + "," + "Current_Members" + "," + current_title_val);
           output_website_directions.push_back(output);
 
         }
 
+        website_index_to_ignore.push_back(j);
+
       } 
+      continue;
     }
 
     if (!name_matched) {
-      std::cout << "Did not find this name in the website!" << std::endl;
-      std::string output("ADD," + current_person_val + "," + " " + "," + " " + "," + "current_members" +"," + current_title_val);
+      std::string output("ADD," + current_person_val + "," + " " + "," + " " + "," + "Current Members" +"," + current_title_val);
       output_website_directions.push_back(output);
     }
 
   }
 
+
+  std::sort(website_index_to_ignore.begin(), website_index_to_ignore.end());
+  //for(auto index: website_index_to_ignore) {
+  //  std::cout << index << std::endl;
+  //}
+  std::cout << std::endl;
+  std::cout << "Place this text into your excel sheet to see changes that need to be made to website:" << std::endl;
+  for (int k = 0; k < website_person_col.size(); k++) {
+    auto website_person_val = website_person_col[k];
+    if(std::count(website_index_to_ignore.begin(), website_index_to_ignore.end(), k) || website_person_col[k].empty()) {
+      continue;
+    }
+    //this list of people are the ones that are on the website already and are no longer a part of the lab
+    std::string output("MOVE,"+website_person_val+","+"Current Members" +","+ website_title_col[k] +","+"Alumni"+","+website_title_col[k]);
+    output_website_directions.push_back(output);
+  }
+
   for(auto value: output_website_directions) {
     std::cout << value << std::endl;
   }
-
 
 
 
